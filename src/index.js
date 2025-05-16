189c512ae9ba0ee610c3eb57df5e001e9ab0ce07
Gustavo Schmidth <gustavo_schmidth@outlook.com.br>
2025-05-15 22:22:38 -0300

    Deploy inicial

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HamburgueriaApp from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HamburgueriaApp />);
